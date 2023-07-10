f821cff3f1cd664c79850d7ca1a967a2cc1518f6
Developer Sajid <mdsajidahmed08@gmail.com>
2023-07-10 19:03:28 +0600

    Instructions files create & comment

---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+// Assignment-2 : Data Type Practice
+
+/* Instructions:
+1. Create a new JavaScript file and save it as "dataTypes.js".
+2. Write JavaScript code to complete the tasks described below.
+3. Test your code by running the "dataTypes.js" file in a web browser or using a JavaScript 
+console. */
+
+/* Tasks:
+1. Declare a variable called "name" and assign your name to it (as a string).
+2. Declare a variable called "age" and assign your age to it (as a number).
+3. Declare a variable called "isStudent" and assign a boolean value (true or false) indicating if 
+you are a student or not.
+4. Declare a variable called "hobbies" and assign an array containing at least three of your 
+hobbies.
+5. Declare a variable called "address" and assign an object with properties "street", "city", and 
+"country" representing your address.
+6. Print the following information using console.log():
+ - Your name: "My name is [name]."
+ - Your age: "I am [age] years old."
+ - Whether you are a student or not: "I am a student: [isStudent]."
+ - Your hobbies: "My hobbies are [hobbies]."
+ - Your address: "I live at [street], [city], [country]." */
+
+/* Example Output:
+My name is John.
+I am 25 years old.
+I am a student: true.
+My hobbies are reading, painting, and hiking.
+I live at 123 Main Street, Cityville, Countryland. */
+
+
+
+
+
+// Assignment-2 : Data Type Practice (Solutions)
---
+++ operators.js
@@ -0,0 +1,32 @@
+// Assignment-3 : Operator Practice
+
+/* Instructions:
+1. Create a new JavaScript file and save it as "operators.js".
+2. Write JavaScript code to complete the tasks described below.
+3. Test your code by running the "operators.js" file in a web browser or using a JavaScript 
+console. */
+
+/* Tasks:
+1. Declare two variables called "num1" and "num2" and assign them any numeric values.
+2. Use the addition operator to calculate the sum of "num1" and "num2". Store the result in a 
+variable called "sum".
+3. Use the subtraction operator to calculate the difference between "num1" and "num2". Store 
+the result in a variable called "difference".
+4. Use the multiplication operator to calculate the product of "num1" and "num2". Store the 
+result in a variable called "product".
+5. Use the division operator to calculate the quotient of "num1" divided by "num2". Store the 
+result in a variable called "quotient".
+6. Use the modulus operator to calculate the remainder when "num1" is divided by "num2". 
+Store the result in a variable called "remainder".
+7. Print the following information using console.log():
+ - The sum of num1 and num2: "The sum is [sum]."
+ - The difference between num1 and num2: "The difference is [difference]."
+ - The product of num1 and num2: "The product is [product]."
+ - The quotient of num1 divided by num2: "The quotient is [quotient]."
+ - The remainder when num1 is divided by num2: "The remainder is [remainder]." */
+
+
+
+ 
+
+// Assignment-3 : Operator Practice (Solutions)
